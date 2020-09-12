570d5e726d8d89e799c50c6ee97ec588fc3d1751
arganaphangquestian <arganaphangquestian@gmail.com>
2020-09-12 15:02:55 +0700

    Generate Popular Response

---
+++ app/src/main/java/dev/falcer/moviedb/data/response/PopularResponse.kt
@@ -0,0 +1,17 @@
+package dev.falcer.moviedb.data.response
+
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class PopularResponse(
+    @Json(name = "page")
+    var page: Int? = 0,
+    @Json(name = "results")
+    var results: List<Movie>? = listOf(),
+    @Json(name = "total_pages")
+    var totalPages: Int? = 0,
+    @Json(name = "total_results")
+    var totalResults: Int? = 0
+)
